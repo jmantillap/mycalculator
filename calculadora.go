c328f00173b8e052d5d6c748ab7e66a949c07573
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-08-01 20:24:53 -0500

    Inicial commit

---
+++ calculadora.go
@@ -0,0 +1,62 @@
+package mycalculator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct {
+}
+
+func (calc) operate(entrada string, operador string) int {
+
+	entradaLimpia := strings.Split(entrada, operador)
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+
+	switch operador {
+	case "+":
+		fmt.Println(operador1 + operador2)
+		return operador1 + operador2
+	case "-":
+		fmt.Println(operador1 - operador2)
+		return operador1 - operador2
+	case "*":
+		fmt.Println(operador1 * operador2)
+		return operador1 * operador2
+	case "/":
+		fmt.Println(operador1 / operador2)
+		return operador1 / operador2
+	default:
+		fmt.Println("Operador no soportado")
+		return 0
+	}
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	operacion := scanner.Text()
+	fmt.Println(operacion)
+	return operacion
+}
+
+func parsear(entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+/*
+func main() {
+	entrada := leerEntrada()
+	operador := leerEntrada()
+	fmt.Println(entrada)
+	fmt.Println(operador)
+	c := calc{}
+	c.operate(entrada, operador)
+
+}
+*/
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jmantillap/mycalculator
+
+go 1.18
